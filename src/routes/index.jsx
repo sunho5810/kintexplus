b59c6928d9175f6e56595300518e69f0f6373771
Sunho Ju <sunho5515@gmail.com>
2023-05-17 17:33:17 +0900

    header분기 완료

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -67,9 +67,11 @@ import EgovAdminPasswordUpdate from 'pages/admin/manager/EgovAdminPasswordUpdate
 import * as EgovNet from 'api/egovFetch'; // jwt토큰 위조 검사 때문에 추가
 import initPage from 'js/ui';
 
+
 const RootRoutes = () => {
   //useLocation객체를 이용하여 정규표현식을 사용한 /admin/~ 으로 시작하는 경로와 비교에 사용(아래 1줄) */}
   const location = useLocation();
+
 
   //리액트에서 사이트관리자에 접근하는 토큰값 위변조 방지용으로 서버에서 비교하는 함수 추가
   const jwtAuthentication = useCallback(() => {
@@ -95,6 +97,7 @@ const RootRoutes = () => {
 
     console.log("------------------------------jwtAuthentication [End]");
     console.groupEnd("jwtAuthentication");
+
   }, []);
 
   //시스템관리 메뉴인 /admin/으로 시작하는 URL은 모두 로그인이 필요하도록 코드추가(아래)
@@ -107,7 +110,7 @@ const RootRoutes = () => {
 		setMounted(true); // 이 값으로 true 일 때만 페이지를 렌더링이 되는 변수 사용.
 		const regex = /^(\/admin\/)+(.)*$/; //정규표현식 사용: /admin/~ 으로 시작하는 경로 모두 포함
 		if(regex.test(location.pathname)) {
-			setMounted(false); // 이 값으로 true 일 때만 페이지를 렌더링이 되는 변수 사용. 기본은 숨기기
+      setMounted(false); // 이 값으로 true 일 때만 페이지를 렌더링이 되는 변수 사용. 기본은 숨기기
 			jwtAuthentication(); // 이 함수에서 관리자단 인증여부 확인 후 렌더링 처리
 		}
 	}
@@ -115,17 +118,24 @@ const RootRoutes = () => {
 
   if(mounted) { // 인증 없이 시스템관리 URL로 접근할 때 렌더링 되는 것을 방지하는 조건추가. 
 	  return (
-	      <Routes>
+
+      <Routes>
+          {console.log("check2222")}
 	        <Route path={URL.ERROR} element={<EgovError />} />
-	        <Route path="*" element={<SecondRoutes/>} />
+	        <Route path="*" element={<SecondRoutes/>}/>
 	      </Routes>
 	  )
   }
+
 }
 
 const SecondRoutes = () => {
 
   const [loginVO, setLoginVO] = useState({});
+
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const location = useLocation();
 
   //useRef객체를 사용하여 페이지 마운트 된 후 ui.js를 로딩 하도록 변경 코드 추가(아래)
   const isMounted = useRef(false); // 아래 로그인 이동 부분이 2번 실행되지 않도록 즉, 마운트 될 때만 실행되도록 변수 생성
@@ -134,12 +144,19 @@ const SecondRoutes = () => {
 		isMounted.current = true; // 이 값으로 true 일 때만 페이지를 렌더링이 되는 변수 사용.
 	}else{
 		initPage();
+    location.pathname.includes("/admin") === true ? setIsAdmin(true) : setIsAdmin(false);
 	}
   },[]);
+
   
+
   return (
     <>
-      <EgovHeader loginUser={loginVO} onChangeLogin={(user) => setLoginVO(user)} />
+      <a
+      {/* {
+        isAdmin ? (<p>admin!!</p>) : (<EgovHeader loginUser={loginVO} onChangeLogin={(user) => setLoginVO(user)} />)
+      } */}
+      
       <Routes>
         {/* MAIN */}
         <Route path={URL.MAIN} element={<EgovMain />} />
